3de69ab22aa1bcfd51f460c10b141acc768184c3
binyamin <binyaminahmed594@gmail.com>
2026-02-19 14:51:01 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About(){
+  return (
+    <div id="about">
+     <h1>About</h1>
+    </div>
+  )
+}
 
 
 function App() {
@@ -24,7 +31,9 @@ function App() {
     <div>
       <NavBar />
       {/* add the <Home> component here */}
+      <Home />
       {/* add your <About> component here */}
+      <About />
     </div>
   );
 }
